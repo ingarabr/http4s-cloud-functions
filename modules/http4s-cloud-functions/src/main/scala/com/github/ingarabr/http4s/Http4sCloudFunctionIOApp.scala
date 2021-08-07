395853455154f7799b92c564a12a9690b1e7e9f8
Nawaar <nawaarpatel00@gmail.com>
2021-08-07 12:41:09 +0100

    cleanup execution context and IORuntime

--- modules/http4s-cloud-functions/src/main/scala/com/github/ingarabr/http4s/Http4sCloudFunctionIOApp.scala
+++ modules/http4s-cloud-functions/src/main/scala/com/github/ingarabr/http4s/Http4sCloudFunctionIOApp.scala
@@ -1,6 +1,6 @@
 package com.github.ingarabr.http4s
 
-import cats.effect.unsafe.{IORuntime, IORuntimeConfig}
+import cats.effect.unsafe.IORuntime
 import cats.effect.IO
 import com.google.cloud.functions.{HttpFunction, HttpRequest, HttpResponse}
 
@@ -8,15 +8,7 @@ trait Http4sCloudFunctionIOApp extends Http4sCloudFunction[IO] with HttpFunction
 
   val defaultChunkSize = 1024
 
-  val (scheduler, _) = IORuntime.createDefaultScheduler()
-
-  implicit val runtime: IORuntime = IORuntime(
-    CatsExecutionContexts.compute,
-    CatsExecutionContexts.blocker,
-    scheduler,
-    () => (),
-    IORuntimeConfig()
-  )
+  implicit val runtime: IORuntime = IORuntime.global
 
   override def service(request: HttpRequest, response: HttpResponse): Unit =
     IO.fromEither(fromRequest(defaultChunkSize, request))
